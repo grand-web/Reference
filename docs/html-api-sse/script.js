29a5d52f006c2095e389c0637289e167a41e228f
grand-web <grand-webdev@mail.ru>
2022-10-19 18:56:35 +0300

    Edit html elements

--- docs/html-api-sse/script.js
+++ docs/html-api-sse/script.js
@@ -1,3 +1,9 @@
-/* empty JS file 
-alert( "Hello, world!" );
-*/
+if (typeof EventSource !== 'undefined') {
+  var source = new EventSource('demo_sse.php');
+  source.onmessage = function (event) {
+    document.getElementById('result').innerHTML += event.data + '<br>';
+  };
+} else {
+  document.getElementById('result').innerHTML =
+    'Sorry, your browser does not support server-sent events...';
+}
